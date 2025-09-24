6579f8cecafaf136874215c69996d23072be24ac
Korbin Boren <bor23020@byui.edu>
2025-09-24 08:40:29 -0600

    hello

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(-1001, 1001);
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("That's Correct! Good Job! :)");
+            }
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+
+        while (true)
+        {
+            Console.WriteLine("Enter a list of number, type 0 when finished.");
+            string input = Console.ReadLine();
+            int Addnumber = int.Parse(input);
+
+            if (Addnumber == 0)
+            {
+                break;
+            }
+            numberList.Add(Addnumber);
+            Console.WriteLine($"Added number {Addnumber}");
+
+        }
+        int sum = numberList.Sum();
+            float avg;
+            if (numberList.Count > 0)
+            {
+                avg = (float)sum / numberList.Count;
+            }
+            else
+            {
+                avg = 0f;
+            }
+        Console.WriteLine($"The Sum of your numbers is {sum}");
+        Console.WriteLine($"The Average of your numbers is {avg}");
     }
 }
